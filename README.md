9b80f2dd92286ba27fd59c18aa1547f4553479a0
kechenghaha <asdpubao@163.com>
2020-06-09 20:26:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#球鞋
+#资料
+[Spring 文档](https://spring.io/guides)    
+[Spring Web](https://spring.io/guides/gs/serving-web-content/)   
+[Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+#工具
+[Git](https://git-scm.com/download)   
---
+++ src/main/java/kecheng/haha/sneakers/SneakersApplication.java
@@ -0,0 +1,13 @@
+package kecheng.haha.sneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakersApplication.class, args);
+    }
+
+}
